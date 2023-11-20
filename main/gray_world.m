edded801df5b9dec750479ea830d090cb31d9765
Alice <alihe684@student.liu.se>
2023-11-20 11:09:05 +0100

    small changes

---
+++ main/gray_world.m
@@ -0,0 +1,23 @@
+function grayworld_img = gray_world(C1)
+
+% Dela upp färgkanalerna
+r = C1(:,:,1); % Röd kanal
+g = C1(:,:,2); % Grön kanal
+b = C1(:,:,3); % Blå kanal
+
+r_avg = mean2(r); 
+g_avg = mean2(g); 
+b_avg = mean2(b); 
+
+% Beräkna faktorer för att balansera kanalerna
+alfa = g_avg/r_avg;
+beta = g_avg/b_avg;
+
+% Justera färgkanalerna med faktorerna
+r_sensor = alfa.*r;
+g_sensor = g;
+b_sensor = beta.*b;
+
+% Visa den justerade bilden
+ grayworld_img = cat(3, r_sensor, g_sensor, b_sensor);
+imshow(greyworld_img)
