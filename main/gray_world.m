1d754ddf4957886f3e5670756fd5a1a0ee06175d
klarabratteby <93648999+klarabratteby@users.noreply.github.com>
2023-11-20 11:34:54 +0100

    eng comments

--- main/gray_world.m
+++ main/gray_world.m
@@ -1,22 +1,22 @@
 function grayworld_img = gray_world(C1)
 
-% Dela upp färgkanalerna
-r = C1(:,:,1); % Röd kanal
-g = C1(:,:,2); % Grön kanal
-b = C1(:,:,3); % Blå kanal
+% Split the color channels
+r = C1(:,:,1); % Red
+g = C1(:,:,2); % Green
+b = C1(:,:,3); % Blue
 
 r_avg = mean2(r); 
 g_avg = mean2(g); 
 b_avg = mean2(b); 
 
-% Beräkna faktorer för att balansera kanalerna
+% Calculate the factors to balance the channels
 alfa = g_avg/r_avg;
 beta = g_avg/b_avg;
 
-% Justera färgkanalerna med faktorerna
+% Adjust the channels with factors
 r_sensor = alfa.*r;
 g_sensor = g;
 b_sensor = beta.*b;
 
-% Visa den justerade bilden
- grayworld_img = cat(3, r_sensor, g_sensor, b_sensor);
+% Result
+grayworld_img = cat(3, r_sensor, g_sensor, b_sensor);
