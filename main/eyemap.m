5410fc80277701f60629e2cad6b463b98d2a8b62
gustafalmesaker <114588941+gustafalmesaker@users.noreply.github.com>
2023-11-20 15:43:52 +0100

    eye and mouth maps added

--- main/eyemap.m
+++ main/eyemap.m
@@ -1,46 +1,46 @@
-function [result] = findeyes(C1)
-C1 = imread('../TNM034/Facial-recognition/db/DB0/db0_1.jpg');
-imshow(C1)
+function [result] = eyemap(C1)
+% C1 = imread('../TNM034/Facial-recognition/db/DB0/db0_1.jpg');
+% imshow(C1)
 C2 = rgb2ycbcr(C1);
-imshow(C2)
+% imshow(C2)
 C3 = double(C2);
-imshow(C3,[])
+% imshow(C3,[])
 
 y=C3(:,:,1);
 cb=C3(:,:,2);
 cr=C3(:,:,3);
 ccb=cb.^2;
-imshow(ccb,[]);
+% imshow(ccb,[]);
 
 ccr=(1-cr).^2;
-imshow(ccr,[]);
+% imshow(ccr,[]);
 
 cbcr=ccb./cr;
-imshow(cbcr,[]);
+% imshow(cbcr,[]);
 
 %EyemapC
 EyemapC=(ccb+ccr+cbcr)/3;
-imshow(EyemapC,[])
+% imshow(EyemapC,[])
 
 %Histogram equalization
 C6=histeq(EyemapC);
-imshow(C6,[])
+% imshow(C6,[])
 
 %EyemapL
 SE=strel('disk',10);
 o=imdilate(y,SE);
 p=1+imerode(y,SE);
 EyemapL=o./p;
-imshow(EyemapL, []);
+% imshow(EyemapL, []);
 
 %Eyemap
 Eyemap = EyemapC.*EyemapL;
-imshow(Eyemap,[]);
+% imshow(Eyemap,[]);
 
 %Dilated & masked
 C8 = strel(10);
 result = imdilate(Eyemap,C8);
-imshow(result,[]);
+% imshow(result,[]);
 
 %Normalizing
 result = (result./max(result(:)));
--- main/mouth_map.m
+++ main/mouth_map.m
@@ -1,33 +1,33 @@
 function result_img = mouth_map(C1)
 
 C2 = rgb2ycbcr(C1);
-imshow(C2)
+% imshow(C2)
 C3 = double(C2);
-imshow(C3,[])
+% imshow(C3,[])
 
 cb=C3(:,:,2);
 cr=C3(:,:,3);
 
 ccr=(cr).^2;
-imshow(ccr,[])
+% imshow(ccr,[])
 
 crcb=cr./cb;
-imshow(crcb,[])
+% imshow(crcb,[])
 
 %Mouth Map
 n = 0.95*mean(cr(:).^2)./(mean(cr(:)./cb(:)));
 
 MouthMap = (ccr).*((ccr - n*(crcb)).^2);
-imshow(MouthMap,[])
+% imshow(MouthMap,[])
 
 % Dilation 
 se = strel(10);
 result_img = imdilate(MouthMap,se);
-imshow(MouthMap,[])
+% imshow(MouthMap,[])
 
 %Diffrence och AND
 result_img=(ccr-crcb).*(MouthMap);
 
 %Normaliz
 result_img=(result_img./max(result_img(:)));
-imshow(result_img,[])
+% imshow(result_img,[])
