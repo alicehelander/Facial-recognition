3f60ee51aa2c46339e93392a3c2afa0d17925f68
gustafalmesaker <114588941+gustafalmesaker@users.noreply.github.com>
2023-11-24 16:02:30 +0100

    imfindcircles does not work properly

--- main/eyemask.m
+++ main/eyemask.m
@@ -1,6 +1,7 @@
 function eye_mask = eyemask(eye_map)
+eye_map = eye_map./(max(max(eye_map)));
 threshold_eye = 0.5;
 eye_mask = eye_map > threshold_eye;
-eyeSE = strel('disk',10);
+eyeSE = strel('disk',5);
 eye_mask = imopen(eye_mask,eyeSE);
 end
--- main/mouthmask.m
+++ main/mouthmask.m
@@ -1,10 +1,12 @@
 function mouth_mask = mouthmask(mouth_map)
+mouth_map = mouth_map./(max(max(mouth_map)));
 
-threshold_mouth = 0.3;
+threshold_mouth = mean(mouth_map);
+
+
+
+
 mouth_mask = mouth_map > threshold_mouth;
 mouthSE = strel('disk',10);
 mouth_mask = imopen(mouth_mask,mouthSE);
-
-
-
 end
