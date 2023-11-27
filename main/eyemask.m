2111e59ecd5449f5b608775bd6627dd135f87f43
gustafalmesaker <114588941+gustafalmesaker@users.noreply.github.com>
2023-11-27 11:10:06 +0100

    eyemask works for image db0-2,3,4

--- main/eyemask.m
+++ main/eyemask.m
@@ -1,15 +1,17 @@
-function eye_mask = eyemask(eye_map)
+function eye_mask = eyemask(eye_map,face_mask)
 eye_map = eye_map./(max(max(eye_map)));
 eye_candidates = 3;
-thresh = 0.9;
+thresh = 0.8;
 
 for i = thresh:-0.1:0
     eye_mask = imbinarize(eye_map,i);
-    eyeSE = strel('disk',10);
+    eyeSE = strel('disk',8);
+    eye_mask = imopen(eye_mask,eyeSE);
     eye_mask = imclose(eye_mask,eyeSE);
+    eye_mask = eye_mask & face_mask;
+    bw = bwboundaries(eye_mask);
 
-    [centers, radii, metric] = imfindcircles(eye_map,[6 60]);
-    if(centers > eye_candidates)
+    if(length(bw) > eye_candidates)
         break
     end
 end
