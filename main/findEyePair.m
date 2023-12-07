25b8ef4c946a035bd0754237d5fa0490af8c2bb8
klarabratteby <93648999+klarabratteby@users.noreply.github.com>
2023-12-07 10:54:12 +0100

    kaos in the house

--- main/findEyePair.m
+++ main/findEyePair.m
@@ -1,15 +1,49 @@
-function [eye1,eye2,angle] = findEyePair(indexX,indexY,mouth_center)
-
+function [eye1,eye2] = findEyePair(indexX,indexY,mouth_center,img)
 % Calculate the distance of each eye center from the mouth center
 eye_centers = [indexX(:) + indexY(:)/2, indexY(:)];
 distances = sqrt(sum((eye_centers - mouth_center).^2, 2));
+% 
+% % Find the indices of the two eyes with the closest distance to the mouth
+% [~, sorted_indices] = sort(distances)
+% selected_eye_indices = [];
+% % Iterate through sorted indices to find the eyes where the mouth index is between them
+% for i = 1:length(sorted_indices)-1
+%     left_eye_index = min(sorted_indices(i), sorted_indices(i+1));
+%     right_eye_index = max(sorted_indices(i), sorted_indices(i+1));
+% 
+%     % Check if both eyes are between the mouth index
+%     if eye_centers(left_eye_index, 1) > x && eye_centers(right_eye_index, 1) > x
+%         selected_eye_indices = [left_eye_index, right_eye_index];
+%         break;  % Break the loop when the condition is satisfied
+%     end
+% end
+% %selected_eye_indices = sorted_indices(1:2)
+% %selected_eye_indices = sorted_indices(find(eye_centers(sorted_indices, 1) < x & eye_centers(sorted_indices, 2) > y, 2));
+% 
+% % Extract the coordinates of the selected eyes
+% selected_eyes = [indexX(selected_eye_indices), indexY(selected_eye_indices)];
+% 
+% viscircles([selected_eyes(1),selected_eyes(3)],10)
+% viscircles([selected_eyes(2),selected_eyes(4)],10)
+% 
+% eye1 = [selected_eyes(1),selected_eyes(3)];
+% eye2 = [selected_eyes(2),selected_eyes(4)];
+% 
+% figure;
+% imshow(img);
+% hold on;
+% viscircles([eye1; eye2], 10, 'EdgeColor', 'r');
+% plot([eye1(1), eye2(1)], [eye1(2), eye2(2)], 'LineWidth', 2, 'Color', 'r')
+face_mid = [size(img, 2) / 2, size(img, 1) / 2]; % Mitt av ansiktet
+eye_distances_to_mid = sqrt((indexX - face_mid(1)).^2 + (indexY - face_mid(2)).^2);
 
-% Find the indices of the two eyes with the closest distance to the mouth
-[~, sorted_indices] = sort(distances);
-selected_eye_indices = sorted_indices(1:2);
+% Sortera ögonen baserat på avståndet till ansiktets mitt
+[sorted_mid_distances, sorted_mid_indices] = sort(eye_distances_to_mid);
 
-% Extract the coordinates of the selected eyes
-selected_eyes = [indexX(selected_eye_indices), indexY(selected_eye_indices)];
+
+% Välj de två ögonen som är närmast ansiktets mitt
+closest_mid_eye_indices = sorted_mid_indices(1:2);
+selected_eyes = [indexX(closest_mid_eye_indices), indexY(closest_mid_eye_indices)];
 
 viscircles([selected_eyes(1),selected_eyes(3)],10)
 viscircles([selected_eyes(2),selected_eyes(4)],10)
@@ -18,83 +52,3 @@ eye1 = [selected_eyes(1),selected_eyes(3)];
 eye2 = [selected_eyes(2),selected_eyes(4)];
 
 
-
-% 
-% % Find the angle between the eyes
-% angle = atan2(0, eye2(1) - eye1(1));
-% 
-% % Check additional conditions for selecting the right eye based on the desired angle
-% if abs(rad2deg(angle)) > 3 && size(selected_eyes, 1) >= 2
-%     for i = 2:length(selected_eye_indices)
-%       % Check if the x-coordinate is within a certain range
-%         x_diff = abs(selected_eyes(i, 1) - eye1(1));
-%         y_diff = abs(selected_eyes(i, 2) - eye1(2));
-% 
-%         if (x_diff > 120) && (x_diff < 150) && (selected_eyes(i, 2) < mouth_center(2)) && (y_diff < 50)
-%             if x_diff > 0 % Check if the selected eye is on the right side
-%                 eye2 = selected_eyes(i, :);
-%                 break;  % Exit the loop once a suitable right eye is found
-%             else
-%                 eye1 = selected_eyes(i, :);
-%             end
-%         end
-%     end
-% end
-% 
-% % Visualize the line connecting the eyes
-% hold on;
-% plot([eye1(1), eye2(1)], [eye1(2), eye2(2)], 'LineWidth', 2, 'Color', 'r');
-% 
-% % end
-% %  % Calculate the distance of each eye center from the mouth center
-% %     eye_centers = [indexX(:) + indexY(:)/2, indexY(:)];
-% %     distances = sqrt(sum((eye_centers - mouth_center).^2, 2));
-% % 
-% %     % Find the indices of the eyes
-% %     [~, sorted_indices] = sort(distances);
-% % 
-% %     % Check additional conditions for selecting the eyes based on the angle
-% %     min_angle = deg2rad(-10);
-% %     max_angle = deg2rad(10);
-% % 
-% %     % Initialize eyes
-% %     eye1 = [];
-% %     eye2 = [];
-% % 
-% %     figure;  % Create a new figure
-% % 
-% %     for i = 1:length(sorted_indices)-1
-% %         % Extract the coordinates of the selected eyes
-% %         selected_eyes = [indexX(sorted_indices(i)), indexY(sorted_indices(i)), ...
-% %                           indexX(sorted_indices(i+1)), indexY(sorted_indices(i+1))];
-% % 
-% %         % Calculate the angle between the eyes
-% %         angle = atan2(selected_eyes(4) - selected_eyes(2), selected_eyes(3) - selected_eyes(1));
-% % 
-% %         % Check if the angle is within the expected range
-% %         if angle > min_angle && angle < max_angle
-% %             % Determine left and right eyes based on x-coordinates
-% %             if selected_eyes(1) < selected_eyes(3)
-% %                 eye1 = selected_eyes(1:2);  % Left eye
-% %                 eye2 = selected_eyes(3:4);  % Right eye
-% %             else
-% %                 eye1 = selected_eyes(3:4);  % Left eye
-% %                 eye2 = selected_eyes(1:2);  % Right eye
-% %             end
-% %             
-% %             % Break the loop once the eyes are found
-% %             break;
-% %         end
-% %     end
-% % 
-% %     % Visualize the final result
-% %     if ~isempty(eye1) && ~isempty(eye2)
-% %         viscircles([eye1; eye2], 10, 'EdgeColor', 'r');
-% %         plot([eye1(1), eye2(1)], [eye1(2), eye2(2)], 'LineWidth', 2, 'Color', 'r');
-% %         title('Detected Eyes and Angle');
-% %     else
-% %         title('No valid eyes detected');
-% %     end
-% % 
-% %     hold off;
-% % end
--- main/iterativeEyeFinder.m
+++ main/iterativeEyeFinder.m
@@ -1,10 +1,10 @@
-function [eye1,eye2] = iterativeEyeFinder(eyes,face_mask,mouth_center)
-for radii = 10:5:25
+function [eye1,eye2] = iterativeEyeFinder(eyes,face_mask,mouth_center,img)
+for radii = 5:1:20
     eye_mask = eyemask(eyes,face_mask,radii);
     [indexX,indexY] = eye_index(eye_mask);
-    [eye1 , eye2] = findEyePair(indexX,indexY,mouth_center);
+    [eye1 , eye2] = findEyePair(indexX,indexY,mouth_center,img);
     if (eye1(1) < mouth_center(1) && eye1(2) > mouth_center(1))  || (eye1(1) > mouth_center(1) && eye1(2) < mouth_center(1))
         break
-    end
+    end 
 end
 end
