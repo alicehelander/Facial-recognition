dfe4754dbe3d13f8acd73db3f4d5d8e8ada16066
Alice <alihe684@student.liu.se>
2023-12-07 11:43:15 +0100

    small changes

--- main/eyemask.m
+++ main/eyemask.m
@@ -5,11 +5,7 @@ thresh = 0.9;
 
 for i = thresh:-0.5:0
     eye_mask = imbinarize(eye_map,i);
-<<<<<<< Updated upstream
     eyeSE = strel('disk',11);
-=======
-    eyeSE = strel('disk',12);
->>>>>>> Stashed changes
     eye_mask = imopen(eye_mask,eyeSE);
     eye_mask = imclose(eye_mask,eyeSE);
     eye_mask = eye_mask & face_mask;
