5ab6e2cc0f3ea790ac63e4ef4490038f060578df
klarabratteby <93648999+klarabratteby@users.noreply.github.com>
2023-12-08 14:04:22 +0100

    test eigenfaces

--- main/test_pca.m
+++ main/test_pca.m
@@ -1,38 +1,28 @@
 function [result] = test_pca(img)
 
-    commonSize = [400, 400];
-    
-    grayScale = rgb2gray(img);
-
-    resizedImg = imresize(grayScale, commonSize);
+size = [400, 400];
+grayscale = rgb2gray(img);
+resized_img = imresize(grayScale, size);
+img_vector = double(resized_img(:));
    
-    imgVector = double(resizedImg(:));
-   
+load('weights.mat', weights)
+load('eigenvalues.mat', eigenValues)
 
-load('.mat', '');
+weights = phi_i' * TrainingSet.eigenVectors;
 
-
-load('','');
-
- 
-    weights = phi_i' * TrainingSet.eigenVectors;
-
-   
-    distances = zeros(size(TrainingSet.weights, 1), 1);
+distances = zeros(size(TrainingSet.weights, 1), 1);
     for i = 1:size(TrainingSet.weights, 1)
         distances(i) = norm(weights - TrainingSet.weights(i, :));
     end
 
-  
-    [~, minIndex] = min(distances);
+[~, minIndex] = min(distances);
 
-  
-    threshold = 500;  
+threshold = 500;  
 
-   
     if distances(minIndex) < threshold
         result = minIndex; 
-    else
-        result = 0;  
+
+        else
+            result = 0;  
     end
 end
