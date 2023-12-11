c18fcbe0eb2ff9d7ef189cff22f74c0492a85b3b
klarabratteby <93648999+klarabratteby@users.noreply.github.com>
2023-12-11 10:30:33 +0100

    not done, just testing

--- main/eigen_faces.m
+++ main/eigen_faces.m
@@ -1,4 +1,4 @@
-% function [meanface, eigenfaces] = eigen_faces(input)
+function [meanface, eigenfaces] = eigen_faces(input)
 % 
 % if size(input, 3) == 3
 %     % Convert color image to grayscale
@@ -54,3 +54,40 @@
 % meanface = u;
 % 
 % end
+
+% 2. Represent the image as a n-vector "X_i"
+M = size(input,3);   % M is the total number of images
+for i = 1:1:M
+    n_vector = reshape(input(:,:,i),1, []);
+    X(i,:) = n_vector(1,:);
+end
+
+
+%"Create Eigenfaces using PCA"
+% 3. Find average face vector, "u" represents the "mean face" for the training data set.
+
+u = double((1/M) * sum(X));
+%imshow(reshape(u,400,[]))
+u = u'; % Transpose u
+
+% 4. Subtract the mean, "u", face for each face vector, "X_i"
+phi = X(:,:) - u;
+
+
+% 5. Find the covariance matrix, C
+A = phi';
+
+% 6b.
+v = transpose(A)*A;
+
+% 6c.
+eigenfaces = A*v;
+
+% for i = 1:1:M
+%     normalize = mat2gray(u_i);
+%     imshow(reshape(normalize(:,i),400,[]))
+% end
+
+meanface = u;
+
+end
