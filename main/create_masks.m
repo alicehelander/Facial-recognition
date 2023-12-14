0f72e9f7b0f8611a1624335908e90916d8f6716b
gustafalmesaker <114588941+gustafalmesaker@users.noreply.github.com>
2023-12-14 16:37:26 +0100

    result images created

--- main/create_masks.m
+++ main/create_masks.m
@@ -1,6 +1,7 @@
 function [face_mask, eye_mask, mouth_mask] = create_masks(img_gw)
 %Takes in the image and creates the 3 different masks
 face_mask = facemask(img_gw);
+
 eyes = eyemap(img_gw);
 eye_mask = eyemask((eyes),face_mask);
 
