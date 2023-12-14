eb40a2296ba9576e154481aadccf84f5c0561438
gustafalmesaker <114588941+gustafalmesaker@users.noreply.github.com>
2023-12-14 10:59:26 +0100

    100% acc

--- main/compute_eigenfaces.m
+++ main/compute_eigenfaces.m
@@ -1,7 +1,39 @@
 function [meanface, eigenVectors, weights] = compute_eigenfaces(X,commonsize)
     % X is a flattened out image (1 x prod(commonsize))
-    meanface = mean(X);
-    centered_data = X - meanface;
-    [eigenVectors,weights] = pca(centered_data);
-    save("eigen_data.mat","eigenVectors","meanface","weights","commonsize","centered_data")
+%     meanface = mean(X);
+%     centered_data = X - meanface;
+%     [eigenVectors,weights] = pca(centered_data);
+% 
+%     for i=1:16
+%         eigenVectors(i,:) = eigenVectors(i,:)/norm(eigenVectors(i,:));
+%     end
+%     save("eigen_data.mat","eigenVectors","meanface","weights","commonsize","centered_data")
+
+
+X = X';
+X = double(X)/255;
+
+max(X)
+meanface = mean(X,2);
+FD = X - meanface;
+
+C = FD' * FD;
+
+A=FD;    % All 16 face differences, mean face substracted
+
+[vi, eValues] = eig(C);
+
+ui=A*vi;        % ui is the eigenface-vectors
+
+eigenVectors = ui;
+
+for i=1:16
+        eigenVectors(:,i) = eigenVectors(:,i)/norm(eigenVectors(:,i));
+end
+
+weights = eigenVectors' * FD;
+
+
+save("eigen_data.mat","eigenVectors","meanface","weights","commonsize")
+
 end
--- main/isPersonInSet.m
+++ main/isPersonInSet.m
@@ -1,65 +1,3 @@
-% function personIndex = isPersonInSet(img, meanface, eigenVectors, commonsize)
-%     load("eigen_data.mat", "weights")
-% 
-%     knownWeights = weights;
-% 
-%     % Normalize the new image
-%     img = normalize_face(img, commonsize);
-%     img = im2gray(img);
-% 
-%     % Ensure img is a column vector
-%     img = double(img(:));
-% 
-%     % Transpose meanface to match the size of img
-%     meanface = meanface';
-% 
-%     % Subtract meanface from img
-%     meanface_diff = img - meanface;
-% 
-%     % Project the new image onto the eigenspace
-%     new_weights = eigenVectors' * meanface_diff;
-% 
-%     % Initialize maximum cosine similarity and personIndex
-%     maxCosineSimilarity = -1;  % initialized to a value less than -1
-%     personIndex = 0;
-% 
-%     % Iterate over each known face and compute cosine similarity
-%     for i = 1:size(knownWeights, 2)
-%         % Compute cosine similarity between the known weights and the weights of the new image
-%         numerator = sum(knownWeights(:, i)' .* new_weights);
-%         denominator = norm(knownWeights(:, i)') * norm(new_weights);
-%         
-%         % Avoid division by zero
-%         if denominator ~= 0
-%             cosineSimilarity = numerator / denominator;
-%         else
-%             cosineSimilarity = 0;
-%         end
-%             fprintf('Cosine Similarity for person %d: %f\n', i, cosineSimilarity);
-% 
-%         % Update the maximum cosine similarity
-%         if cosineSimilarity > maxCosineSimilarity
-%             maxCosineSimilarity = cosineSimilarity;
-%             personIndex = i
-%         end
-%     end
-% 
-%     % Set a threshold for matching based on cosine similarity
-%     threshold = -0.8;  % You can adjust this threshold
-% 
-%     % Check if the maximum cosine similarity is above the threshold
-%     if maxCosineSimilarity > threshold
-%         % If a match is found, return the index of the most similar person
-%         return;
-%     else
-%         % If no match found, return -1
-%         personIndex = -1;
-%     end
-% end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
 function personIndex = isPersonInSet(img, meanface, eigenVectors, commonsize)
     load("eigen_data.mat", "weights")
 
@@ -70,39 +8,45 @@ function personIndex = isPersonInSet(img, meanface, eigenVectors, commonsize)
     img = im2gray(img);
 
     % Ensure img is a column vector
-    img = double(img(:));
+    img = double(img(:))/255;
 
     % Transpose meanface to match the size of img
-    meanface = meanface';
+%     meanface = meanface';
 
     % Subtract meanface from img
     meanface_diff = img - meanface;
+   
 
+    
+    
     % Project the new image onto the eigenspace
     new_weights = eigenVectors' * meanface_diff;
-
+    
     % Initialize minimum distance and personIndex
     minDistance = inf;
     personIndex = 0;
 
-    % Iterate over each known face and compute distances
-    for i = 1:size(knownWeights, 2)
-        % Compute distances between the known weights and the weights of the new image
-        distances = sqrt(sum((knownWeights(:, i)' - new_weights).^2, 1));
-%         distances = abs(sum(sum((knownWeights(:, i)' - new_weights), 1)))
-        % Find the minimum distance
-        currentMinDistance = min(distances);
 
-        % Check if the current minimum distance is below the threshold
-        if currentMinDistance < minDistance
-            minDistance = currentMinDistance;
-            personIndex = i;
-        end
+   % Iterate over each known face and compute distances
+for i = 1:size(knownWeights, 2)
+    % Compute distances between the known weights and the weights of the new image
+    distances = sqrt(sum((knownWeights(:,i) - new_weights).^2, 1));
+%     distances = sqrt(sum((knownWeights(i, :) - new_weights).^2, 1));
+    % Find the minimum distance and its index
+    currentMinDistance = min(distances);
+
+    % Check if the current minimum distance is below the threshold
+    if currentMinDistance < minDistance
+        minDistance = currentMinDistance;
+        personIndex = i; % Update to use minIndex instead of i
     end
+end
+
    
     % Set a distance threshold for matching
-    threshold = 3.0e+04;
-
+    threshold = 22;
+    
+    
     % Check if the minimum distance is below the threshold
     if minDistance < threshold
         % Return the index of the most similar person
@@ -112,49 +56,3 @@ function personIndex = isPersonInSet(img, meanface, eigenVectors, commonsize)
         personIndex = 0;
     end
 end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% function personIndex = isPersonInSet(img, meanface, eigenVectors, commonsize)
-%    
-%     % Normalize the new image
-%     img = normalize_face(img, commonsize);
-%     img = im2gray(img);
-%     
-%     % Ensure img is a column vector
-%     img = double(img(:));
-%     
-%     % Transpose meanface to match the size of img
-%     meanface = meanface';
-%     
-%     % Subtract meanface from img
-%     img = img - meanface;
-% 
-%     % Project the new image onto the eigenspace
-%     weights = eigenVectors' * img;
-%     
-%     % Calculate distances to each image in the set
-%     weighted_image = img - eigenVectors * weights;
-% 
-% 
-%     whos weighted_image
-%     whos eigenVectors
-% 
-%     distances = sqrt(sum((eigenVectors(:,1) .* (weighted_image)).^2, 1));
-%     for i = 2:16
-%         distances = sqrt(sum((eigenVectors(:,i) .* weighted_image).^2, 1));
-%     end
-%     % Check if the minimum distance is below the threshold
-%     [minDistance, personIndex] = min(distances);
-% 
-%     % Set a distance threshold for matching
-%     threshold = 0.1;
-%     % Check if the minimum distance is below the threshold
-%     if minDistance < threshold
-%         % Return the index of the most similar person
-%         return;
-%     else
-%         % If no match found, return 0
-%         personIndex = 0;
-%     end
-% end
